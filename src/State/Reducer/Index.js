f6ac544a8ba9395b8866ae671e9b0aeb9da38dd1
zabirK89 <zabirk89@gmail.com>
2022-02-08 11:00:36 +0530

    3 files modified

--- src/State/Reducer/Index.js
+++ src/State/Reducer/Index.js
@@ -1,11 +1,11 @@
 import { combineReducers } from "redux";
 import Amountreducer from "./Amountreducer";
-import transactionReducer from "./transactionReducer";
+
 
 
 const reducers=combineReducers({
     amount :Amountreducer,
-    transactions:transactionReducer
+ 
 })
 
 export default reducers
--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import { store } from './State/Store';
+import { Provider } from 'react-redux';
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+<Provider store={store}>
+<App />
+</Provider>
+, document.getElementById("root"));
